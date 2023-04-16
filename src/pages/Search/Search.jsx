8cc3809bc6732c6005c5167a1cc0999d5723add8
Bruno H Cardoso <riquebruno@yahoo.com.br>
2023-04-16 12:47:06 -0300

    Requisito 05 Ok. Feito campo de pesquisa com a ação de ativar o botão de pesquisa apenas quando tem 2 ou mais caracteres

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -2,10 +2,63 @@ import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 
 class Search extends Component {
+  state = {
+    artist: '',
+    button: true,
+    isLoading: false,
+  };
+
+  onInputChange = () => {
+    const { artist } = this.state;
+    const min = 2;
+    if (artist.length >= min) {
+      this.setState({
+        button: false,
+      });
+    } else {
+      this.setState({
+        button: true,
+      });
+    }
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({
+      artist: value,
+    }, this.onInputChange);
+  };
+
+  btnClick = async () => {
+    // farei a seguir
+  };
+
   render() {
+    const { button, isLoading } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <header>
+          <div>
+            <input
+              onChange={ this.handleChange }
+              type="text"
+              name="artist"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+            />
+            <button
+              onClick={ this.btnClick }
+              type="button"
+              data-testid="search-artist-button"
+              disabled={ button }
+            >
+              Pesquisar
+            </button>
+          </div>
+        </header>
+        <body>
+          {isLoading ? (<p>Carregando...</p>) : (<article>a</article>)}
+        </body>
       </div>
     );
   }
